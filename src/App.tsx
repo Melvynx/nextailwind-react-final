dba208c1c7b5b87c0023558dee9ecf0115702b20
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-07-22 10:24:11 +0200

    nt-6.shadcn-dashbaord-button-variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./dashboard/Button";
 import { DashboardContent } from "./dashboard/DashboardContent";
 import { DashboardContentHeader } from "./dashboard/DashboardContentHeader";
 import { DashboardHeader } from "./dashboard/DashboardHeader";
@@ -5,6 +6,17 @@ import { DashboardHeader } from "./dashboard/DashboardHeader";
 export const App = () => {
   return (
     <div className="dark min-h-full bg-background text-foreground">
+      <div className="mx-auto flex gap-4 p-8">
+        <Button variant="default" size="sm">
+          Default
+        </Button>
+        <Button variant="default">Default</Button>
+        <Button variant="default" size="lg">
+          Default
+        </Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="outline">Outline</Button>
+      </div>
       <DashboardHeader />
       <DashboardContentHeader />
       <DashboardContent />
--- src/dashboard/Button.tsx
+++ src/dashboard/Button.tsx
@@ -1,8 +1,38 @@
-import { PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef } from "react";
+import { cn } from "../cn";
 
-export const Button = (props: PropsWithChildren) => {
+type Variant = "default" | "ghost" | "outline";
+
+type Size = "default" | "sm" | "lg";
+
+export const Button = ({
+  variant = "default",
+  size = "default",
+  className,
+  ...props
+}: ComponentPropsWithoutRef<"button"> & {
+  variant?: Variant;
+  size?: Size;
+}) => {
   return (
-    <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+    <button
+      className={cn(
+        "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-transparent",
+        {
+          "text-foreground bg-primary hover:bg-primary/90":
+            variant === "default",
+          "hover:bg-accent": variant === "ghost",
+          "border-border hover:bg-accent": variant === "outline",
+        },
+        {
+          "h-10 px-4 py-2": size === "default",
+          "h-9 rounded-md px-3": size === "sm",
+          "h-11 rounded-md px-8": size === "lg",
+        },
+        className
+      )}
+      {...props}
+    >
       {props.children}
     </button>
   );
